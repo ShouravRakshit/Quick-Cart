c3bcdd8035e669e3811136261538debc833544a3
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-12-09 12:06:25 -0700

    created the view functions for the cart.

--- ecommercesite/cart/views.py
+++ ecommercesite/cart/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
+from .models import Cart, CartItem
+from django.views.decorators.http import require_POST
 
 # Create your views here.
+
+@require_POST
+def cart(request, product_id):
+    cart_id = request.session.get('cart_id')
+
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create()
+    else:
+        cart = Cart.objects.create()
+        request.session['cart_id'] = cart.id
+
+    product = get_object_or_404(Product, id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+
+    response_data = {  
+        "success": True,
+        "message": f"Added {product.name} to cart",
+    }
+
+    return JsonResponse(response_data)
+    
+def cart_detail(request):
+    cart_id = request.session.get('cart_id')
+    cart = None
+
+    if cart_id:
+        cart = Cart.objects.get(id=cart_id)
+
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
