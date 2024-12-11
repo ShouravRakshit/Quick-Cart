cf3a1fa2f43ebfeabd314df4e5c54deb07e86fd0
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-12-11 01:51:49 -0700

    created the templates for orders

--- ecommercesite/orders/models.py
+++ ecommercesite/orders/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 from django.urls import reverse
 
-from ecommercesite.products.models import Product
+from products.models import Product
 
 class Order(models.Model):
     first_name = models.CharField(max_length=150)
--- ecommercesite/orders/views.py
+++ ecommercesite/orders/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+
+from cart.models import Cart
+from orders.forms import OrderCreateForm
+from orders.models import OrderItem
 
 # Create your views here.
+
+def order_create(request):
+    cart = None
+    cart_id = request.session.get('cart_id')
+    if cart_id:
+        cart = get_object_or_404(Cart, id=cart_id)
+
+        if not cart or not cart.items.all():
+            return redirect('cart:cart_detail')
+        
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.save()
+            for item in cart.items.all():
+                OrderItem.objects.create(
+                    order=order, 
+                    product=item.product,
+                    price=item.product.price, 
+                    quantity=item.quantity)
+            cart.delete()
+            del request.session['cart_id']
+            return redirect('orders:order_confirmation', order.id)
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/order_create.html', {'cart': cart, 'form': form})
