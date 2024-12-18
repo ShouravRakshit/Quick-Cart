1b147ab241eba2af4c9d41db123ce24d976ccb8f
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-12-17 19:39:15 -0700

    renamed the project to quickcart

--- QuickCart/quickcart/asgi.py
+++ QuickCart/quickcart/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommercesite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickcart.settings')
 
 application = get_asgi_application()
--- QuickCart/quickcart/wsgi.py
+++ QuickCart/quickcart/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommercesite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickcart.settings')
 
 application = get_wsgi_application()
